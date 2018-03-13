0809ef7fb8279f3f5d436282fc8baadf47b54a93
Sylar.bigb <28239337@qq.com>
2018-03-13 10:05:29 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title', 'Simple App') - Laravel Project</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@enxtends('layouts.default')
+@section('title','Home')
+@section('content')
+    <h1>主页</h1>
+@stop

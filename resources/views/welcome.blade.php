ad478a39892cee9ba2b09721f2a414685582c849
vagrant <vagrant@homestead>
2018-03-13 09:38:08 +0000

    test push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    WHY???????
+                    hi, im here again
                 </div>
 
                 <div class="links">
